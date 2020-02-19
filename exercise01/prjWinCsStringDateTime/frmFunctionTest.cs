aa6acd279bff0063b4d037aaa0de23e5cab7296e
sky-lu <honglin.lu.sr@gmail.com>
2020-02-18 22:24:25 -0500

    functions tests correct

--- exercise01/prjWinCsStringDateTime/frmFunctionTest.cs
+++ exercise01/prjWinCsStringDateTime/frmFunctionTest.cs
@@ -17,6 +17,57 @@ namespace prjWinCsStringDateTime
             InitializeComponent();
         }
         
+        //private string getLastName (string email)
+        //{
+        //    string lname;
+        //    lname = email.Substring(0, email.IndexOf("."));
+        //    return lname.Trim();
+        //}
+        //private string getFirstName (string email)
+        //{
+        //    string fname;
+        //    fname = email.Substring(email.IndexOf(".") + 1, email.IndexOf("@") - email.IndexOf(".") - 1);
+        //    return fname.Trim();
+        //}
+        //private string getCompany (string email)
+        //{
+        //    string comp;
+        //    int posA = email.IndexOf("@");
+        //    int posP2 = email.IndexOf(".", posA);
+        //    comp = email.Substring(posA + 1, posP2 - posA - 1);
+        //    return comp.Trim();
+        //}
+        //private string toUpLower(string anyString)
+        //{
+        //    return anyString.Substring(0, 1).ToUpper() + anyString.Substring(1).ToLower();
+        //}
+        //private string getGenderTitle()
+        //{
+        //    return (radMale.Checked) ? "Sir" : "Miss";
+        //}
+        //private int CalculateAge(Int32 CurrentYear, Int32 YearBorn)
+        //{
+        //    return CurrentYear - YearBorn;
+        //}
+        //private decimal GetSalary(string function)
+        //{
+        //    switch (function)
+        //    {
+        //        case "Analyst":
+        //            return 80000;
+        //        case "Programmer":
+        //            return 50000;
+        //        case "Secretary":
+        //            return 30000;
+        //    }
+        //    return 0;
+
+
+
+
+        }
+
+        //---------------------------------------------------------------------------------------------------------------
         // a function to calculate age
         private Int32 GetAge(DateTime Birth)
         {
@@ -141,6 +192,25 @@ namespace prjWinCsStringDateTime
             // display the position and salary
             lblResult.Text += " and position (" + combFunction.SelectedItem.ToString() + "), your salary is ";
             lblResult.Text += GetSalary(combFunction.SelectedItem.ToString());
+
+
+
+
+            //--------------------------------------------------------------------------------------------
+            //string email = txtEmail.Text.Trim();
+            //string lname = getLastName(email);
+            //string fname = getFirstName(email);
+            //string comp = getCompany(email);
+            //lname = toUpLower(lname);
+            //fname = toUpLower(fname);
+            //comp = toUpLower(comp);
+            //int age = CalculateAge(DateTime.Today.Year, dtpBirth.Value.Year);
+            //decimal salary = GetSalary(combFunction.SelectedItem.ToString());
+            //string result = getGenderTitle() + fname + " " + lname + ",\n ";
+            //result += "At" + comp + ",with your age (" + age + "),\n";
+            //result += "And position (" + combFunction.SelectedItem.ToString() + "),\n";
+            //result += "Your salary is" + salary + ".\n";
+            //lblResult.Text = result;
         }
 
         private void frmFunctionTest_Load(object sender, EventArgs e)
