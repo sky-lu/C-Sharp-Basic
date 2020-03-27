3aad1b1ff8b899ea11fe72fb18d276c78dacd579
sky-lu <honglin.lu.sr@gmail.com>
2020-03-26 20:37:22 -0400

    Midterm

---
+++ exercise01/prjMidtermAP1HonglinLu/EmployeesManagement.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace prjMidtermAP1HonglinLu
+{
+    partial class EmployeesManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstSelectNumber = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(301, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EMPLOYEES-MANAGEMENT";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(148, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(753, 166);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add an Empolyee by Email";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(643, 78);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(83, 31);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(83, 78);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(535, 30);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(397, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Firstname.Lastname.Number@Company.com";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.lstSelectNumber);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(148, 282);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(753, 365);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search an Manage an Employee";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnCancel);
+            this.groupBox3.Controls.Add(this.btnSave);
+            this.groupBox3.Controls.Add(this.btnDelete);
+            this.groupBox3.Controls.Add(this.btnEdit);
+            this.groupBox3.Controls.Add(this.txtCompany);
+            this.groupBox3.Controls.Add(this.txtLname);
+            this.groupBox3.Controls.Add(this.txtFname);
+            this.groupBox3.Controls.Add(this.txtNumber);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(227, 62);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(508, 259);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Selected Employee Informations";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(370, 198);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(119, 42);
+            this.btnCancel.TabIndex = 11;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(370, 138);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(119, 42);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(370, 85);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(119, 42);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(370, 29);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(119, 42);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Location = new System.Drawing.Point(149, 178);
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(205, 30);
+            this.txtCompany.TabIndex = 7;
+            // 
+            // txtLname
+            // 
+            this.txtLname.Location = new System.Drawing.Point(149, 138);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(205, 30);
+            this.txtLname.TabIndex = 6;
+            // 
+            // txtFname
+            // 
+            this.txtFname.Location = new System.Drawing.Point(149, 97);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(205, 30);
+            this.txtFname.TabIndex = 5;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(149, 54);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(205, 30);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 24);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Company :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 24);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Last Name :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 24);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "First Name :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Number :";
+            // 
+            // lstSelectNumber
+            // 
+            this.lstSelectNumber.FormattingEnabled = true;
+            this.lstSelectNumber.ItemHeight = 24;
+            this.lstSelectNumber.Location = new System.Drawing.Point(41, 82);
+            this.lstSelectNumber.Name = "lstSelectNumber";
+            this.lstSelectNumber.Size = new System.Drawing.Size(158, 220);
+            this.lstSelectNumber.TabIndex = 1;
+            this.lstSelectNumber.SelectedIndexChanged += new System.EventHandler(this.lstSelectNumber_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Select Number";
+            // 
+            // EmployeesManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 982);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "EmployeesManagement";
+            this.Text = "Lasalle College Mid Term Evaluation";
+            this.Load += new System.EventHandler(this.EmployeesManagement_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstSelectNumber;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ exercise01/prjMidtermAP1HonglinLu/EmployeesManagement.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace prjMidtermAP1HonglinLu
+{
+    public partial class EmployeesManagement : Form
+    {
+        public EmployeesManagement()
+        {
+            InitializeComponent();
+        }
+
+        struct Employee
+        {
+            public String Number;
+            public String Fname;
+            public String Lname;
+            public String Company;
+        }
+        Employee[] tabEmplos = new Employee[20];
+        Int16 current, nbEmplos;
+
+
+        private Int16 FileToArray()
+        {
+            StreamReader myFile = new StreamReader("The text file Employees.txt");
+            Int16 i = 0;
+            while (myFile.EndOfStream == false)
+            {
+                String number = myFile.ReadLine();
+                String fname = myFile.ReadLine();
+                String lname = myFile.ReadLine();
+                String company = myFile.ReadLine();
+                tabEmplos[i].Number = number;
+                tabEmplos[i].Fname = fname;
+                tabEmplos[i].Lname = lname;
+                tabEmplos[i].Company = company;
+                i++;
+            }
+            myFile.Close();
+            return i;
+        }
+        private bool Exist(String number)
+        {
+            foreach (Employee em in tabEmplos)
+            {
+                if (em.Number == number)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void lstSelectNumber_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Int32 indx = lstSelectNumber.SelectedIndex;
+            if (indx != -1)
+            {
+                txtNumber.Text = tabEmplos[indx].Number;
+                txtFname.Text = tabEmplos[indx].Fname;
+                txtLname.Text = tabEmplos[indx].Lname;
+                txtCompany.Text = tabEmplos[indx].Company;
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            txtFname.Focus();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to delete this employee?", "Employee Information", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                txtNumber.Text = txtFname.Text = txtLname.Text = txtCompany.Text = "";
+                lstSelectNumber.Items.Remove(lstSelectNumber.SelectedItem);
+            }
+            for (Int32 j = lstSelectNumber.SelectedIndex; j <= nbEmplos; j++)
+            {
+                if (j != -1)
+                {
+                    tabEmplos[j] = tabEmplos[j + 1];
+                }
+            }
+            nbEmplos--;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Int32 indx = lstSelectNumber.SelectedIndex;
+            if (indx != -1)
+            {
+                txtFname.Text = txtFname.Text.Substring(0, 1).ToUpper() + txtFname.Text.Substring(1).ToLower();
+                txtLname.Text = txtLname.Text.Substring(0, 1).ToUpper() + txtLname.Text.Substring(1).ToLower();
+                txtCompany.Text = txtCompany.Text.Substring(0, 1).ToUpper() + txtCompany.Text.Substring(1).ToLower();
+
+                tabEmplos[indx].Fname = txtFname.Text;
+                tabEmplos[indx].Lname = txtLname.Text;
+                tabEmplos[indx].Company = txtCompany.Text;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Int32 indx = lstSelectNumber.SelectedIndex;
+            if (indx != -1)
+            {
+                txtFname.Text = tabEmplos[indx].Fname;
+                txtLname.Text = tabEmplos[indx].Lname;
+                txtCompany.Text = tabEmplos[indx].Company;
+            }
+        }
+
+       
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            String email, Lname, Fname, Company, Number;
+            int posD1, posA, posD2, posD3;
+            email = txtEmail.Text.Trim();
+            if (email.Length == 0)
+            {
+                MessageBox.Show("Please enter Email !");
+                txtEmail.Focus();
+                return;
+            }
+            posD1 = email.IndexOf(".");
+            if (posD1 == -1)
+            {
+                MessageBox.Show("Please enter dot !");
+                txtEmail.Focus();
+                return;
+            }
+            Fname = email.Substring(0, posD1).Trim();
+            Fname = Fname.Substring(0, 1).ToUpper() + Fname.Substring(1).ToLower();
+
+            if (Fname.Length == 0)
+            {
+                MessageBox.Show("Please enter Firstname !");
+                txtEmail.Focus();
+                return;
+            }
+            posD2 = email.IndexOf(".", posD1+1);
+            if (posD2 == -1)
+            {
+                MessageBox.Show("Please enter another dot !");
+                txtEmail.Focus();
+                return;
+            }
+            Lname = email.Substring(posD1 + 1, posD2 - posD1 - 1).Trim();
+            Lname = Lname.Substring(0, 1).ToUpper() + Lname.Substring(1).ToLower();
+            if (Lname.Length == 0)
+            {
+                MessageBox.Show("Please enter Lastname !");
+                txtEmail.Focus();
+                return;
+            }
+            posA = email.IndexOf("@");
+            if (posA == -1)
+            {
+                MessageBox.Show("Please enter @");
+                txtEmail.Focus();
+                return;
+            }
+            Number = email.Substring(posD2 + 1, posA - posD2 - 1).Trim();
+            Number = Number.ToUpper();
+            if (Number.Length == 0)
+            {
+                MessageBox.Show("Please enter number !");
+                txtEmail.Focus();
+                return;
+            }
+            posD3 = email.IndexOf(".", posA);
+            if (posD3 == -1)
+            {
+                MessageBox.Show("Please enter another dot !");
+                txtEmail.Focus();
+                return;
+            }
+            Company = email.Substring(posA + 1, posD3 - posA - 1).Trim();
+            Company = Company.Substring(0, 1).ToUpper() + Company.Substring(1).ToLower();
+            if (Company.Length == 0)
+            {
+                MessageBox.Show("Please enter Company !");
+                txtEmail.Focus();
+                return;
+            }
+
+
+            if (nbEmplos < 20)
+            {
+                String number = Number;
+                if (Exist(number))
+                {
+                    MessageBox.Show("Student already exist in list !");
+                    return;
+                }
+                else
+                {
+                    Employee tmp;
+                    tmp.Number = Number;
+                    tmp.Fname = Fname;
+                    tmp.Lname = Lname;
+                    tmp.Company = Company;
+                    
+                    tabEmplos[nbEmplos] = tmp;
+                    txtEmail.Text = "";
+                    
+                    nbEmplos++;
+                    lstSelectNumber.Items.Add(Number);
+                }
+
+            }
+
+            StreamWriter myFile = new StreamWriter("The text file Employees.txt",true);
+            for(Int16 i = 0;i < nbEmplos; i++)
+            {
+                myFile.WriteLine(tabEmplos[i].Number);
+                myFile.WriteLine(tabEmplos[i].Fname);
+                myFile.WriteLine(tabEmplos[i].Lname);
+                myFile.WriteLine(tabEmplos[i].Company);
+            }
+            myFile.Close();
+
+        }
+
+        public void EmployeesManagement_Load(object sender, EventArgs e)
+        {
+            nbEmplos = FileToArray();
+            if (nbEmplos != 0)
+            {
+                for (Int16 indx = 0; indx < nbEmplos; indx++)
+                {
+                    lstSelectNumber.Items.Add(tabEmplos[indx].Number);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("These is no employee !");
+            }
+
+        }
+    }
+}
