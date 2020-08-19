503e7a4602e9a385f34e98813f8ab5eea07b3e3a
sky-lu <honglin.lu.sr@gmail.com>
2020-08-18 23:30:40 -0400

    connect database

--- prjASPC.MVC/Blog/Controllers/PostsController.cs
+++ prjASPC.MVC/Blog/Controllers/PostsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Collections;
+using Microsoft.AspNetCore.Http;
 
 namespace Blog.Controllers
 {
@@ -26,6 +29,18 @@ namespace Blog.Controllers
         public IActionResult Add()
         {
             return View("Add");
+        }
+
+        public Boolean Submit(string PostTitle)
+        {
+            //string postTitle = collection["PostTitle"];
+            //string postContext = collection["PostContext"];
+            string myInsertQuery = "INSERT INTO users (email, password) Values({PostTitle}, 'lalllalalal')";
+
+            System.Console.WriteLine(PostTitle);
+            //System.Console.WriteLine(postContext);
+
+            return true;
         }
     }
 }
--- prjASPC.MVC/Blog/Controllers/SigninController.cs
+++ prjASPC.MVC/Blog/Controllers/SigninController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace Blog.Controllers
 {
     public class SigninController : Controller
     {
+
+
         public IActionResult Index()
         {
             return View();
         }
+
+        //public Boolean test()
+        //{
+        //    string myConnectionString = "Database=blog_db;Data Source=localhost;User Id=root;Password=";
+        //    MySqlConnection myConnection = new MySqlConnection(myConnectionString);
+        //    string myInsertQuery = "INSERT INTO users (id, email, password) Values(12, 'xuming@gmail.com', 'lalllalalal')";
+        //    MySqlCommand myCommand = new MySqlCommand(myInsertQuery);
+        //    myCommand.Connection = myConnection;
+        //    myConnection.Open();
+        //    myCommand.ExecuteNonQuery();
+        //    myCommand.Connection.Close();
+        //    System.Console.WriteLine("hello world");
+        //    return true;
+        //}
     }
 }
