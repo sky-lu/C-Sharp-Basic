6c715de6fa08f5d57080d89a6c0fe54433d250c6
sky-lu <honglin.lu.sr@gmail.com>
2020-08-24 23:26:50 -0400

    contact list page

--- prjASPC.MVC/Blog/Controllers/ContactController.cs
+++ prjASPC.MVC/Blog/Controllers/ContactController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace Blog.Controllers
 {
@@ -11,6 +12,28 @@ namespace Blog.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public void ContactSubmit(string name, string email, string phone, string message)
+        {
+            string myConnectionString = "Database=blog_db;Data Source=localhost;User Id=root;Password=";
+            MySqlConnection myConnection = new MySqlConnection(myConnectionString);
+
+            string myInsertQuery = "Insert into contacts (name, email, phone, message, user_id) " +
+                $"Values ('{name}', '{email}', '{phone}', '{message}', 0)";
+
+            MySqlCommand myCommand = new MySqlCommand(myInsertQuery);
+            myCommand.Connection = myConnection;
+            myConnection.Open();
+            myCommand.ExecuteNonQuery();
+            myCommand.Connection.Close();
+
+            Response.Redirect("/Contact/Index");
         }
     }
 }
