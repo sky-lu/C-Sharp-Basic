c5e7bfbc394e37b53212128c946b8c9624d4d1ac
sky-lu <honglin.lu.sr@gmail.com>
2020-09-01 23:58:28 -0400

    edit page function & Lab1

---
+++ Multi-Tire Application/Lab1_ConnectedModel/BLL/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_ConnectedModel.BLL
+{
+    class Employee
+    {
+        private int employeeId;
+        private string firstName;
+        private string lastName;
+        private string jobTitle;
+
+        public int EmployeeId { get => employeeId; set => employeeId = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+
+        public string LastName { get => lastName; set => lastName = value; }
+
+        public string JobTitle { get => jobTitle; set => jobTitle = value; }
+
+    }
+}
