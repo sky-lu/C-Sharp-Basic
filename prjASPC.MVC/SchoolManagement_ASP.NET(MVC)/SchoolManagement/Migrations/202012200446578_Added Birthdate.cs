5293c45cd1a4f63259690e2cdf4a9fc1bbf9f03f
Honglin <honglin.lu.sr@gmail.com>
2020-12-20 00:19:12 -0500

    add user data points

---
+++ prjASPC.MVC/SchoolManagement_ASP.NET(MVC)/SchoolManagement/Migrations/202012200446578_Added Birthdate.cs
@@ -0,0 +1,18 @@
+namespace SchoolManagement.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedBirthdate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "BirthDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "BirthDate");
+        }
+    }
+}
