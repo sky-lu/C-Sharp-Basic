226e9ffa3c2a12c28b7bd053457297122f96f69b
Honglin <honglin.lu.sr@gmail.com>
2020-12-20 23:07:10 -0500

    control user access

--- prjASPC.MVC/SchoolManagement_ASP.NET(MVC)/SchoolManagement/Startup.cs
+++ prjASPC.MVC/SchoolManagement_ASP.NET(MVC)/SchoolManagement/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using SchoolManagement.Models;
 
 [assembly: OwinStartupAttribute(typeof(SchoolManagement.Startup))]
 namespace SchoolManagement
@@ -9,6 +12,36 @@ namespace SchoolManagement
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+        }
+
+        public void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser
+                {
+                    UserName = "admin",
+                    Email = "admin@schmng.com"
+                };
+
+                var password = "password";
+
+                var usr = userManager.Create(user, password);
+                if (usr.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Admin");
+                }
+
+            }
         }
     }
 }
