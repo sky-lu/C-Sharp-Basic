c0c95fb3b6867dbee5b34e671d497c09fbc78f85
Honglin <honglin.lu.sr@gmail.com>
2020-12-22 22:27:33 -0500

    implement jquery UI Autocomplete

--- prjASPC.MVC/SchoolManagement_ASP.NET(MVC)/SchoolManagement/App_Start/BundleConfig.cs
+++ prjASPC.MVC/SchoolManagement_ASP.NET(MVC)/SchoolManagement/App_Start/BundleConfig.cs
@@ -10,6 +10,9 @@ namespace SchoolManagement
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+           
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,6 +28,11 @@ namespace SchoolManagement
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/jqueryui").Include(
+                "~/Content/themes/base/all.css",
+                "~/Content/themes/base/autocomplete.css"));
+                
         }
     }
 }
