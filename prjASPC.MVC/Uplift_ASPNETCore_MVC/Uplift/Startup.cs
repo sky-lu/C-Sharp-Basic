9898c682a061c1609eb8f19236d610510c89dcfd
Honglin <honglin.lu.sr@gmail.com>
2021-01-10 22:55:54 -0500

    create category API & category datatable

--- prjASPC.MVC/Uplift_ASPNETCore_MVC/Uplift/Startup.cs
+++ prjASPC.MVC/Uplift_ASPNETCore_MVC/Uplift/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Uplift.DataAccess.Data;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.DataAccess.Data.Repository;
 
 namespace Uplift
 {
@@ -30,9 +32,12 @@ namespace Uplift
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddControllersWithViews().AddNewtonsoftJson().AddRazorRuntimeCompilation();
             services.AddRazorPages();
         }
 
